dd1e137dcd06963d4368324bc1912093b70c71fb
gurkankizik <gurkankizik@hotmail.com>
2024-07-28 18:59:33 +0300

    GameStore.Api added

---
+++ GameStore.Api/Endpoints/GenreEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using GameStore.Api.Entities;
+using GameStore.Api.Mapping;
+using Microsoft.EntityFrameworkCore;
+using GameStore.Api.Data;
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GenreEndpoints
+{
+    public static RouteGroupBuilder MapGenresEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/genres");
+
+        group.MapGet("/", async (GameStoreContext dbContext) =>
+            await dbContext.Genre.Select(Genre => Genre.ToDto()).AsNoTracking().ToListAsync()
+        );
+        return group;
+    }
+}
